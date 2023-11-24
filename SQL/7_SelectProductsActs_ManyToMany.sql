c188de451e29d0f86df8c89e24ba5c3e7dc5926c
Demanrusss <deman.russs@gmail.com>
2023-11-25 00:50:17 +0600

    Added SQL scripts

---
+++ SQL/7_SelectProductsActs_ManyToMany.sql
@@ -0,0 +1,15 @@
+﻿IF OBJECT_ID(N'SelectProductsActsSelectedSampleQuantities', N'U') IS NULL
+BEGIN
+
+CREATE TABLE SelectProductsActsSelectedSampleQuantities
+(
+    [SelectProductsActId] INT NOT NULL,
+	[SelectedSampleQuantityId] INT NOT NULL,
+    CONSTRAINT PK_SelectProductsActsSelectedSampleQuantities PRIMARY KEY (SelectProductsActId, SelectedSampleQuantityId),
+    CONSTRAINT FK_SelectProductsActsSelectedSampleQuantities_SelectProductsActId FOREIGN KEY (SelectProductsActId)
+		REFERENCES SelectProductsActs(Id),
+	CONSTRAINT FK_SelectProductsActsSelectedSampleQuantities_SelectedSampleQuantityId FOREIGN KEY (SelectedSampleQuantityId)
+		REFERENCES SelectedSampleQuantities(Id)
+)
+
+END
---
+++ SQL/8_ExpertDecisions_ManyToMany.sql
@@ -0,0 +1,15 @@
+﻿IF OBJECT_ID(N'ExpertDecisionsTestProtocols', N'U') IS NULL
+BEGIN
+
+CREATE TABLE ExpertDecisionsTestProtocols
+(
+    [ExpertDecisionId] INT NOT NULL,
+	[TestProtocolId] INT NOT NULL,
+    CONSTRAINT PK_ExpertDecisionsTestProtocols PRIMARY KEY (ExpertDecisionId, TestProtocolId),
+    CONSTRAINT FK_ExpertDecisionsTestProtocols_ExpertDecisionId FOREIGN KEY (ExpertDecisionId)
+		REFERENCES ExpertDecisions(Id),
+	CONSTRAINT FK_ExpertDecisionsTestProtocols_TestProtocolId FOREIGN KEY (TestProtocolId)
+		REFERENCES TestProtocols(Id)
+)
+
+END
